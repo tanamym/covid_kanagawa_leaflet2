1fad2115d3a2a8ac38c31de4ddb33e039d78785d
tanamym <66995058+tanamym@users.noreply.github.com>
2021-12-16 00:07:53 +0900

    変更

--- server.R
+++ server.R
@@ -46,22 +46,32 @@ library(mapview)
 if (!require(magick)) {
     install.packages("magick")
 }
-library(magick)
-
+library(stringr)
 if (!require(stringr)) {
   install.packages("stringr")
 }
-library(stringr)
+library(magick)
 if (!require(webshot)) {
     install.packages("webshot")
+   
+}
+# library(rgdal)
+# if (!require(rgdal)) {
+#   install.packages("rgdal")
+#   
+# }
+library(sf)
+if (!require(sf)) {
+  install.packages("sf")
 
 }
-webshot :: install_phantomjs()
+#webshot :: install_phantomjs()
 
 
 
-
+# Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
+    #コロナのデータ読み込み
   data2020 <-
     fread("https://raw.githubusercontent.com/tanamym/covid19_colopressmap_isehara/main/data2020.csv",encoding="UTF-8")
   
@@ -76,98 +86,146 @@ shinyServer(function(input, output, session) {
     fread("https://raw.githubusercontent.com/tanamym/covid19_colopressmap_isehara/main/yoko_covid.csv",encoding="UTF-8") %>%
     mutate(Fixed_Date=as.Date(Date),
            Residential_City=City)
-  xy<-read.csv("xy.csv",encoding = "SHIFT-JIS")
-  list<-read.csv("list.csv",encoding = "SHIFT-JIS")%>%
-    rename("X1"="X","Y1"="Y")
+  
   data7 <-
     rbind(data2020,data202106,data202109,data2021) %>%
     mutate(Fixed_Date=as.Date(Fixed_Date)) %>%
     arrange(desc(Fixed_Date),Hos,hos)%>%
-    count(Fixed_Date,Residential_City,hos)%>%
+    count(Fixed_Date,Residential_City,hos,X,Y)%>%
     full_join(ycd%>%
                 mutate(hos="yokohama"))%>%
     mutate(Residential_City=ifelse(!is.na(City),City,Residential_City)) %>%
     mutate(n=ifelse(!is.na(City),count,n))%>%
-    arrange(Fixed_Date)%>%
-    left_join(xy,by=c("Residential_City"="City"))%>%
-    left_join(list,by=c("Residential_City"="list"))%>%
-    mutate(X=ifelse(is.na(X),X1,X),
-           Y=ifelse(is.na(Y),Y1,Y))
+    arrange(Fixed_Date)
   date <- 
     data.frame(Date=min(data7$Fixed_Date):max(data7$Fixed_Date)) %>%
     arrange(desc(Date)) %>%
     mutate(Date=as.Date(Date,origin="1970-01-01")) %>%
     filter(Date>="2020-04-20")
-  # data7<-
-  #   fread("https://raw.githubusercontent.com/tanamym/covid19_colopressmap_isehara/main/coviddata.csv",encoding = "UTF-8")%>%
-  #   mutate(Fixed_Date=as.Date(Fixed_Date))%>%
-  #   filter(!is.na(X))
-  # date<-
-  #   data7%>%
-  #   data.frame()%>%
-  #   arrange(desc(Fixed_Date))%>%
-  #   distinct(Fixed_Date)
-    output$date<-
-      renderUI({
-        dateInput("x",
-                  label = h5("累積日数の最後の日付入力"),
-                  max = date[1,1],
-                  value = date[1,1])
-      })
-    
+  xy<-read.csv("xy.csv",encoding = "SHIFT-JIS")
+  output$date<-
+    renderUI({
+      dateInput("x",
+                label = h5("累積日数の最後の日付入力"),
+                max = date[1,1],
+                value = date[1,1])
+    })
+  jinko<-read.csv("jinko.csv",fileEncoding = "SJIS")
+  jinko<-data.frame(jinko)
 
-    jinko<-read.csv("jinko.csv")
-    jinko<-data.frame(jinko)
+  shp<-
+    read_sf("N03-190101_14_GML/N03-19_14_190101_2.shp",options = "ENCODING=CP932")
 
     output$covid_map <- renderLeaflet({
         #ここ書き換える
         switch (input$button,
-                leaflet1 = { date<-lubridate::ymd(input$x)-input$y
+                leaflet1 = { 
+                  x<-input$x
+                  y<-input$y
+                  if(is.null(x)){
+                    x<-max(date$Date)
+                    y<-14
+                  }
+                  date<-lubridate::ymd(x)-y+1
                 #集計
-                data7.1<-data7%>%
-                    filter(Fixed_Date>=date,Fixed_Date<=lubridate::ymd(input$x))%>%
-                    group_by(Residential_City,X,Y)%>%
-                    summarise(count=n())%>%
-                    filter(X>0,Y>0)
+                data7.1<-
+                  data7%>%
+                  filter(Fixed_Date>=date,
+                           Fixed_Date<=lubridate::ymd(x))%>%
+                  group_by(Residential_City,X,Y)%>%
+                  summarise(count=n())%>%
+                  ungroup()%>%
+                  filter(X>0,Y>0)%>%
+                  full_join(xy,by=c("Residential_City"="City"))%>%
+                  mutate(count=ifelse(is.na(X),0,count))%>%
+                  mutate(N03_004=Residential_City)
 
-                #leafletの可視化
-                leaflet(data7.1) %>% addTiles() %>%
-                    addProviderTiles(providers$CartoDB.Positron) %>%
-                    #setView(lng=139.4725,lat=35.4478,zoom=10)%>%
-                    fitBounds(lng1=139.124343, lat1=35.117843, lng2=139.652899, lat2=35.665052)%>%
-                    addCircleMarkers(~X, ~Y, stroke=FALSE,
-                                     radius =sqrt(data7.1$count)*input$en,
-                                     label = ~htmlEscape(Residential_City),
-                                     labelOptions = labelOptions(direction = 'auto',noHide = T, textOnly = TRUE,textsize = "10px"))%>%
-                    addCircleMarkers(~X, ~Y, stroke=FALSE,
-                                     radius =sqrt(data7.1$count)*input$en,
-                                     label = ~htmlEscape(count),
-                                     labelOptions = labelOptions(direction = 'bottom',noHide = T, textOnly = TRUE,textsize = "10px"),
-                    )%>%addControl(tags$div(HTML(paste(date,lubridate::ymd(input$x),sep = "~")))  , position = "topright") },
+                data7.2<-
+                  sp::merge(shp, data7.1,
+                            by="N03_004", all=F,duplicateGeoms = TRUE)
+                head(data7.2)
+                #色設定
+                pal <- colorNumeric(palette=c("white","red"),domain=c(0,input$color*y), reverse=F)
+         
+                pal2<-
+                  data7.2%>%
+                  mutate(col=pal(count),
+                         col2=ifelse(count>input$color*y,"red",col),
+                         flag=ifelse(count>input$color,paste0(input$color*y,"~"),paste0(count%/%10*10,"~")))
+                
+                data7.2%>%
+                  leaflet() %>%
+                  #fitBounds(lng1=139.124343, lat1=35.117843, lng2=139.652899, lat2=35.665052)%>% 
+                  setView(lng=139.424343, lat=35.417843,zoom=10)%>%
+                  addProviderTiles(providers$CartoDB.Positron) %>% 
+                  addPolygons(fillOpacity = 1,
+                              weight=1,
+                              color = "#666",
+                              fillColor = ~pal2$col2,
+                              label = paste0(data7.2$N03_004,data7.2$count))%>%
+                  addLegend(data=pal2%>%distinct(flag,.keep_all = T)%>%arrange(count),
+                            position="bottomright",
+                            pal=pal,
+                            values = c(0,input$color*y),
+                            #color=~col2,labels=~flag,
+                            title = "累積感染者数",opacity = 1,
+                            #labFormat = labelFormat(transform = function(x)x*x)
+                            )%>%
+                  addControl(tags$div(HTML(paste(date,lubridate::ymd(x),sep = "~")))  , position = "topright")
+                },
                 leaflet2={
-                    date<-lubridate::ymd(input$x)-input$y
+                  x<-input$x
+                  y<-input$y
+                  if(is.null(x)){
+                    x<-max(date$Date)
+                    y<-14
+                  }
+                    date<-lubridate::ymd(x)-y+1
                     #集計
                     data7.1<-data7%>%
-                        filter(Fixed_Date>=date,Fixed_Date<=lubridate::ymd(input$x))%>%
+                        filter(Fixed_Date>=date,Fixed_Date<=lubridate::ymd(x))%>%
                         group_by(Residential_City,X,Y)%>%
                         summarise(count=n())%>%
-                        filter(X>0,Y>0)
+                        filter(X>0,Y>0)%>%
+                      full_join(xy,by=c("Residential_City"="City"))%>%
+                      mutate(count=ifelse(is.na(X),0,count))
                     jinko2<-left_join(data7.1,jinko,by=c("Residential_City"="City"))
                     jinko3<-jinko2%>%
-                        mutate(count_j=count/jinko*100000)
-                    leaflet(jinko3) %>% addTiles() %>%
-                        addProviderTiles(providers$CartoDB.Positron) %>%
-                        #setView(lng=139.4825,lat=35.4478,zoom=10)%>%
-                        fitBounds(lng1=139.124343, lat1=35.117843, lng2=139.652899, lat2=35.665052)%>%
-                        addCircleMarkers(~X, ~Y, stroke=FALSE,
-                                         radius =sqrt(jinko3$count_j)*input$en,
-                                         label = ~htmlEscape(Residential_City),
-                                         labelOptions = labelOptions(direction = 'auto',noHide = T, textOnly = TRUE,textsize = "10px"))%>%
-                        addCircleMarkers(~X, ~Y, stroke=FALSE,
-                                         radius =sqrt(jinko3$count_j)*input$en,
-                                         label = ~htmlEscape(round(count_j,digits = 4)),
-                                         labelOptions = labelOptions(direction = 'bottom',noHide = T, textOnly = TRUE,textsize = "10px")
-                        )%>%addControl(tags$div(HTML(paste(date,lubridate::ymd(input$x),sep = "~")))  , position = "topright")
+                        mutate(count_j=count/jinko*100000)%>%
+                      mutate(N03_004=Residential_City)
+                    
+                    data7.2<-
+                      sp::merge(shp, jinko3,
+                                by="N03_004", all=F,duplicateGeoms = TRUE)
+                    #色設定
+                    pal <- colorNumeric(palette=c("white","red"),domain=c(0,input$color*y), reverse=F)
+                    pal2<-
+                      data7.2%>%
+                      mutate(col=pal(count_j),
+                             col2=ifelse(count_j>input$color*y,"red",col),
+                             flag=ifelse(count_j>input$color,paste0(input$color*y,"~"),paste0(count_j%/%10*10,"~")))
+                    data7.2%>%
+                      leaflet() %>%
+                      setView(lng=139.424343, lat=35.417843,zoom=10)%>%
+                      #fitBounds(lng1=139.124343, lat1=35.117843, lng2=139.652899, lat2=35.665052)%>% 
+                      addProviderTiles(providers$CartoDB.Positron) %>% 
+                      addPolygons(fillOpacity = 1,
+                                  weight=1,
+                                  color = "#666",
+                                  #labelOptions = labelOptions(noHide = T, textOnly = TRUE),
+                                  fillColor = ~pal2$col2,
+                                  label = paste0(data7.2$N03_004,round(data7.2$count_j,2))
+                                  )%>%
+                      addLegend(data=pal2%>%distinct(flag,.keep_all = T)%>%arrange(count_j)
+                                ,position="bottomright",
+                                pal=pal,
+                                values = c(0,input$color*y),
+                                #color=~col2,labels=~flag,
+                                title = "10万人当たりの累積感染者数",opacity = 1,
+                                #labFormat = labelFormat(transform = function(x)x*x)
+                      )%>%
+                      addControl(tags$div(HTML(paste(date,lubridate::ymd(x),sep = "~")))  , position = "topright")
+                   
                     }
                 )
     }
--- ui.R
+++ ui.R
@@ -1,43 +1,87 @@
 
+if (!require(htmltools)) {
+  install.packages("htmltools")
+}
+library(htmltools)
 if (!require(shiny)) {
   install.packages("shiny")
 }
 library(shiny)
-
-
+if (!require(dplyr)) {
+  install.packages("dplyr")
+}
+library(dplyr)
+if (!require(data.table)) {
+  install.packages("data.table")
+}
+library(data.table)
 if (!require(leaflet)) {
   install.packages("leaflet")
 }
 library(leaflet)
+if (!require(lubridate)) {
+  install.packages("lubridate")
+}
+library(lubridate)
+if (!require(readxl)) {
+  install.packages("readxl")
+}
+library(readxl)
+if (!require(rsconnect)) {
+  install.packages("rsconnect")
+}
+library(rsconnect)
+if (!require(mapview)) {
+    install.packages("mapview")
+}
+library(mapview)
+
+if (!require(magick)) {
+    install.packages("magick")
+}
+library(magick)
+if (!require(webshot)) {
+    install.packages("webshot")
+ 
+}
+library(rgdal)
+if (!require(rgdal)) {
+  install.packages("rgdal")
+  
+}
+#webshot :: install_phantomjs()
+
 
 shinyUI(fluidPage(
 # Application title
     titlePanel("COVID-MAP"),
     
-
+    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(h3("入力"),
                      h4("地図の設定"),
                      uiOutput("date"),
+                     #dateInput("x",label = h5("累積日数の最後の日付入力"),max=date[1,1],value = date[1,1]),#Sys.Date()),
                      numericInput("y",label = h5("終了日までの累積日数を入力"),value="14"),
                      h6("日付を6/30、日数を14にすると、6/16~6/30の累積感染者マップが出力されます。"),
+                     h5("横浜市は市単位での表示になっています。（区単位ではありません。）"),
                      radioButtons("button",label = "グラフの種類を選択してください",
                                   c("累積感染者数"="leaflet1",
                                     "10万人当たりの累積感染者数"="leaflet2")
                                   ),
-                     sliderInput("en",label = "円のサイズの指定",
-                                 min = 1,
-                                 max = 10,
-                                 value = 5)
-                    
+                     sliderInput("color",
+                                 label = "色の調整(一日当たり）",
+                                 min = 10,
+                                 max = 200,
+                                 value = 10,)
                      
                      ),
         
         
         
         # Show a plot of the generated distribution
-        mainPanel(
-            leafletOutput("covid_map"),
+        mainPanel(h4("出力"),
+            leafletOutput("covid_map",height = "500px"),
             
         )
     )
